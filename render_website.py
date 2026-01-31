c533efb1535f3c9b87b3409149e8773967a7cd1c
tomaa-dev <lajpanovatoma@gmail.com>
2026-01-31 22:37:11 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -2,24 +2,25 @@ import json
 import pprint
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server, shell
 
 
 def read_file():
-    pass
-
-def main():
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-
-    template = env.get_template('template.html')
-
     with open("meta_data.json", "r", encoding="utf-8") as file:
         meta_data = file.read()
 
     books = json.loads(meta_data)
-    pprint.pprint(books)
+    return books
+
+
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+
+    books = read_file()
 
     rendered_page = template.render(
         books=books,
@@ -28,8 +29,12 @@ def main():
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+
+def main():  
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.', host='127.0.0.1', port=5500)
 
 
 if __name__ == '__main__':
